5ca91987be5a87871b4f71f5fe5b8132991a202f
AHerda <adrianherda@gmail.com>
2022-12-13 14:12:38 +0100

    Pocatki sewera

--- src/main/java/serwer/Serwer.java
+++ src/main/java/serwer/Serwer.java
@@ -1,9 +1,31 @@
 package serwer;
 
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class Serwer {
-  private Gracz gracze;
+  private List<Gracz> gracze;
+  private List<Pokoj> pokoje;
   private ServerSocket ss;
+
+  public void main(String[] args) {
+
+    try {
+      ss = new ServerSocket(6666);
+
+      while(true) {
+        Socket socket = ss.accept();
+
+        InputStream input = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+        OutputStream output = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(output, true);
+      }
+    } catch (IOException ioe) {
+      System.out.println("IO exception");
+    }
+  }
 }
