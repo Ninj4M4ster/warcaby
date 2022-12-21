6c634aadf858d51097ddb1764ce656b3ffc5db32
Jakub Drzewiecki <kuba.drzewiecki@icloud.com>
2022-12-21 01:31:18 +0100

    Ukończyłem tworzenie planszy zarówno po stronie białych, jak i czarnych pionków. Brakuje tylko pionków do przesuwania

--- src/main/java/klient/model/ModelGry.java
+++ src/main/java/klient/model/ModelGry.java
@@ -1,5 +1,47 @@
 package klient.model;
 
-public class ModelGry implements Model {
+import javafx.scene.Parent;
+import javafx.scene.layout.GridPane;
 
+public class ModelGry implements Model {
+  private int iloscPol_ = 8;
+  private String kolorPionkow_ = "bialy";
+  private GridPane planszaGry_ = new GridPane();
+  private Parent[][] polaPlanszy_;
+  private String[] znakiIndeksow_ = {"A", "B", "C", "D", "E", "F", "G", "H"};
+
+  public int iloscPol() {
+    return this.iloscPol_;
+  }
+
+  public void ustawIloscPol(int ilosc) {
+    this.iloscPol_ = ilosc;
+    String[] indeksy = new String[this.iloscPol_];
+    for(int i=0; i < this.iloscPol_; i++) {
+      indeksy[i] = String.valueOf('A' + i);
+    }
+    this.znakiIndeksow_ = indeksy;
+  }
+
+  public String kolorPionkow() {
+    return this.kolorPionkow_;
+  }
+
+  public void ustawKolorPionkow(String kolor) {
+    if(!kolor.equals("bialy") && !kolor.equals("czarny"))
+      throw new IllegalArgumentException("Jedyne mozliwe kolory pionkow to 'bialy' oraz 'czarny'");
+    this.kolorPionkow_ = kolor;
+  }
+
+  public GridPane planszaGry() {
+    return this.planszaGry_;
+  }
+
+  public void ustawPolaPlanszy(Parent[][] listaPol) {
+    this.polaPlanszy_ = listaPol;
+  }
+
+  public String[] znakiIndeksow() {
+    return this.znakiIndeksow_;
+  }
 }
--- src/main/java/klient/widoki/WidokGry.java
+++ src/main/java/klient/widoki/WidokGry.java
@@ -4,9 +4,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -20,6 +20,7 @@ public class WidokGry implements Widok {
   private KontrolerGry kontroler_;
   private StackPane okno_;
   private GridPane planszaGry_;
+  private static final double SZEROKOSC_INDEKSOW = 4;
 
   @Override
   public Parent utworzWidok(KontrolerWidoku kontroler, Model model) {
@@ -31,6 +32,7 @@ public class WidokGry implements Widok {
 
     this.okno_ = new StackPane();
     this.okno_.setAlignment(Pos.CENTER);
+    this.okno_.setBackground(Background.fill(Color.valueOf("#242424")));
 
     this.utworzPlanszeGry();
 
@@ -39,67 +41,36 @@ public class WidokGry implements Widok {
 
   public void utworzPlanszeGry() {
     // plansza gry
-    planszaGry_ = new GridPane();
+    planszaGry_ = this.model_.planszaGry();
     planszaGry_.setStyle("-fx-background-color: #222222;");
     planszaGry_.maxHeightProperty().bind(this.okno_.heightProperty().multiply(0.98));
     planszaGry_.maxWidthProperty().bind(planszaGry_.maxHeightProperty());
     planszaGry_.setAlignment(Pos.CENTER);
 
-    // ustawienie ograniczen na kolumny
-    utworzDodajOgraniczenieKolumny(5);
-    utworzDodajOgraniczenieRzedu(5);
+    int iloscPol = this.model_.iloscPol();
 
-    for(int i=0; i < 8; i++) {
-      utworzDodajOgraniczenieKolumny(11.25);
-      utworzDodajOgraniczenieRzedu(11.25);
+    // ustawienie ograniczen na kolumny
+    utworzDodajOgraniczenieKolumny(SZEROKOSC_INDEKSOW);
+    utworzDodajOgraniczenieRzedu(SZEROKOSC_INDEKSOW);
+
+    for(int i=0; i < iloscPol; i++) {
+      utworzDodajOgraniczenieKolumny((100.0 - SZEROKOSC_INDEKSOW) / iloscPol);
+      utworzDodajOgraniczenieRzedu((100.0 - SZEROKOSC_INDEKSOW) / iloscPol);
     }
 
-    utworzDodajOgraniczenieKolumny(5);
-    utworzDodajOgraniczenieRzedu(5);
+    utworzDodajOgraniczenieKolumny(SZEROKOSC_INDEKSOW);
+    utworzDodajOgraniczenieRzedu(SZEROKOSC_INDEKSOW);
 
     // indeksy pol
     this.utworzDodajIndeksyPlanszy();
 
     // dodanie do planszy gry kafelkow w odpowiednich kolorach
-    // TODO(Jakub Drzewiecki): Zmienić na dodawanie rzędów do listy i potem na podstawie parametru
-    //  wyznaczajacego kolor pionkow wyświetlać rzędy z listy od początku lub od końca
-    for(int i=1; i <= 8; i+=2) {
-      for(int j=1; j <= 8; j += 2) {
-        StackPane kafelek = new StackPane();
-        kafelek.setBackground(Background.fill(Color.BLACK));
-
-        Label opis = new Label(i + " - " + j);
-        kafelek.getChildren().add(opis);
-
-        planszaGry_.add(kafelek, i, j);
-
-        kafelek = new StackPane();
-        kafelek.setBackground(Background.fill(Color.BEIGE));
-
-        opis = new Label(i + " - " + (j + 1));
-        kafelek.getChildren().add(opis);
-
-        planszaGry_.add(kafelek, i, j + 1);
-      }
-      for(int j=1; j <= 8; j += 2) {
-        StackPane kafelek = new StackPane();
-        kafelek.setBackground(Background.fill(Color.BEIGE));
-
-        Label opis = new Label((i + 1) + " - " + j);
-        kafelek.getChildren().add(opis);
-
-        planszaGry_.add(kafelek, i + 1, j);
-
-        kafelek = new StackPane();
-        kafelek.setBackground(Background.fill(Color.BLACK));
-
-        opis = new Label((i + 1) + " - " + (j + 1));
-        kafelek.getChildren().add(opis);
-
-        planszaGry_.add(kafelek, i + 1, j + 1);
-      }
-    }
-
+    Parent[][] listaPol = this.stworzListePolPlanszy();
+    this.model_.ustawPolaPlanszy(listaPol);
+    if(this.model_.kolorPionkow().equals("bialy"))
+      this.wyswietlWidokBialy(listaPol);
+    else
+      this.wyswietlWidokCzarny(listaPol);
 
     this.okno_.getChildren().add(planszaGry_);
   }
@@ -118,33 +89,157 @@ public class WidokGry implements Widok {
     planszaGry_.getRowConstraints().add(rzad);
   }
 
+  // TODO(Jakub Drzewiecki): Dosyć długa ta funkcja,
+  //  trzeba się zastanowić czy nie da się jej jakoś sensownie uprościć
   private void utworzDodajIndeksyPlanszy() {
+    int iloscPol = this.model_.iloscPol();
     // utworzenie naroznikow
-    planszaGry_.add(this.utworzNaroznikPlanszy(), 0, 0);
-    planszaGry_.add(this.utworzNaroznikPlanszy(), 9, 9);
-    planszaGry_.add(this.utworzNaroznikPlanszy(), 0, 9);
-    planszaGry_.add(this.utworzNaroznikPlanszy(), 9, 0);
+    planszaGry_.add(this.utworzNaroznikPlanszy(Color.valueOf("#5c3600")), 0, 0);
+    planszaGry_.add(this.utworzNaroznikPlanszy(Color.valueOf("#5c3600")), iloscPol + 1, iloscPol + 1);
+    planszaGry_.add(this.utworzNaroznikPlanszy(Color.valueOf("#382100")), 0, iloscPol + 1);
+    planszaGry_.add(this.utworzNaroznikPlanszy(Color.valueOf("#382100")), iloscPol + 1, 0);
 
     // utworzenie indeksow z wymaganymi opisami
-    for(int i=1; i < 9; i++) {
-      planszaGry_.add(this.utworzIndeksPlanszy(0 + " - " + i), 0, i);
-      planszaGry_.add(this.utworzIndeksPlanszy(9 + " - " + i), 9, i);
-      planszaGry_.add(this.utworzIndeksPlanszy(i + " - " + 0), i, 0);
-      planszaGry_.add(this.utworzIndeksPlanszy(i +  " - " + 9), i, 9);
+    String[] znakiIndeksow = this.model_.znakiIndeksow();
+    for(int i=1; i < iloscPol + 1; i+=2) {
+      if(this.model_.kolorPionkow().equals("bialy")) {
+        // normalna kolejnosc wyswietlania indeksow
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(znakiIndeksow[iloscPol - i], Color.valueOf("#382100")),
+            0,
+            i);
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(znakiIndeksow[iloscPol - i - 1], Color.valueOf("#5c3600")),
+            0,
+            i + 1);
+
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(znakiIndeksow[iloscPol - i], Color.valueOf("#5c3600")),
+            iloscPol + 1,
+            i);
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(znakiIndeksow[iloscPol - i - 1], Color.valueOf("#382100")),
+            iloscPol + 1,
+            i + 1);
+
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(String.valueOf(i), Color.valueOf("#382100")),
+            i,
+            0);
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(String.valueOf(i + 1), Color.valueOf("#5c3600")),
+            i + 1,
+            0);
+
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(String.valueOf(i), Color.valueOf("#5c3600")),
+            i,
+            iloscPol + 1);
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(String.valueOf(i+1), Color.valueOf("#382100")),
+            i + 1, iloscPol + 1);
+      } else {
+        // odwrocona kolejnosc wyswietlania indeksow
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(znakiIndeksow[i-1], Color.valueOf("#382100")),
+            0,
+            i);
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(znakiIndeksow[i], Color.valueOf("#5c3600")),
+            0,
+            i + 1);
+
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(znakiIndeksow[i-1], Color.valueOf("#5c3600")),
+            iloscPol + 1,
+            i);
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(znakiIndeksow[i], Color.valueOf("#382100")),
+            iloscPol + 1,
+            i + 1);
+
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(
+                String.valueOf(iloscPol - i + 1), Color.valueOf("#382100")),
+            i,
+            0);
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(
+                String.valueOf(iloscPol - i), Color.valueOf("#5c3600")),
+            i + 1,
+            0);
+
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(
+                String.valueOf(iloscPol - i + 1), Color.valueOf("#5c3600")),
+            i,
+            iloscPol + 1);
+        planszaGry_.add(
+            this.utworzIndeksPlanszy(
+                String.valueOf(iloscPol - i), Color.valueOf("#382100")),
+            i + 1, iloscPol + 1);
+      }
     }
   }
 
-  private StackPane utworzNaroznikPlanszy() {
+  private StackPane utworzNaroznikPlanszy(Color kolor) {
     StackPane rog = new StackPane();
-    rog.setBackground(Background.fill(Color.BROWN));
+    rog.setBorder(Border.stroke(Color.valueOf("#2e2e2e")));
+    rog.setBackground(Background.fill(kolor));
     return rog;
   }
 
-  private StackPane utworzIndeksPlanszy(String opis) {
+  private StackPane utworzIndeksPlanszy(String opis, Color kolor) {
     StackPane indeks = new StackPane();
+    indeks.setBorder(Border.stroke(Color.valueOf("#2e2e2e")));
+
     Label opisIndeksu = new Label(opis);
+    opisIndeksu.setTextFill(Color.valueOf("#bababa"));
+
     indeks.getChildren().add(opisIndeksu);
-    indeks.setBackground(Background.fill(Color.BROWN));
+
+    indeks.setBackground(Background.fill(kolor));
     return indeks;
+  }
+
+  private Parent[][] stworzListePolPlanszy() {
+    int iloscPol = this.model_.iloscPol();
+    Parent[][] listaPol = new Parent[iloscPol][iloscPol];
+    for(int i=0; i < iloscPol; i+=2) {
+      for(int j=0; j < iloscPol; j += 2) {
+        listaPol[i][j] = this.utworzPolePlanszy(Color.BEIGE);
+        listaPol[i][j+1] = this.utworzPolePlanszy(Color.BLACK);
+      }
+      for(int j=0; j < iloscPol; j += 2) {
+        listaPol[i+1][j] = this.utworzPolePlanszy(Color.BLACK);
+        listaPol[i+1][j+1] = this.utworzPolePlanszy(Color.BEIGE);
+      }
+    }
+    return listaPol;
+  }
+
+  private StackPane utworzPolePlanszy(Color kolor) {
+    StackPane kafelek = new StackPane();
+    kafelek.setBackground(Background.fill(kolor));
+    kafelek.setAlignment(Pos.CENTER);
+    return kafelek;
+  }
+
+  private void wyswietlWidokBialy(Parent[][] listaPol) {
+    int iloscPol = this.model_.iloscPol();
+    for(int i=0; i < iloscPol; i++) {
+      for(int j=0; j < iloscPol; j++) {
+        this.planszaGry_.add(listaPol[i][j], i + 1, j + 1);
+      }
+    }
+  }
+
+  private void wyswietlWidokCzarny(Parent[][] listaPol) {
+    int iloscPol = this.model_.iloscPol();
+    for(int i=0; i < iloscPol; i++) {
+      for(int j=0; j < iloscPol; j++) {
+        this.planszaGry_.add(listaPol[iloscPol - i - 1][iloscPol - j - 1], i + 1, j + 1);
+      }
+    }
   }
 }
