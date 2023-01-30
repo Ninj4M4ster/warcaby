b5f445dc6152c2a12e8c26eb10dc4f635de4d6fd
AHerda <adrianherda@gmail.com>
2023-01-31 00:53:07 +0100

    wstep do dostepnych ruchow v2

--- src/main/java/serwer/komendy/PodajDostepneRuchy.java
+++ src/main/java/serwer/komendy/PodajDostepneRuchy.java
@@ -28,12 +28,16 @@ public class PodajDostepneRuchy extends ZasadyGry implements Komenda {
     }
     @Override
     public String Wykonaj(String reszta, Pokoj pokoj) {
-        for(int x = 0; x < pokoj.getPlansza().length; x += 1) {
-            for(int y = 0; y < pokoj.getPlansza().length; y += 1) {
-                if(pokoj.kontroler_stanu_gry.getStan() == KontrolerStanuGry.StanGry.RUCH_BIALYCH && pokoj.getPlansza()[x][y] % 2 == 1) {
+        if(pokoj.kontroler_stanu_gry.getStan() == KontrolerStanuGry.StanGry.RUCH_BIALYCH) {
+            for (int x = 0; x < pokoj.getPlansza().length; x += 1) {
+                for (int y = 0; y < pokoj.getPlansza().length; y += 1) {
 
                 }
-                else if(pokoj.kontroler_stanu_gry.getStan() == KontrolerStanuGry.StanGry.RUCH_CZARNYCH && pokoj.getPlansza()[x][y] % 2 == 0 && pokoj.getPlansza()[x][y] != 0) {
+            }
+        }
+        else if (pokoj.kontroler_stanu_gry.getStan() == KontrolerStanuGry.StanGry.RUCH_CZARNYCH) {
+            for (int x = 0; x < pokoj.getPlansza().length; x += 1) {
+                for (int y = 0; y < pokoj.getPlansza().length; y += 1) {
 
                 }
             }
@@ -41,188 +45,17 @@ public class PodajDostepneRuchy extends ZasadyGry implements Komenda {
         return "true";
     }
 
-    private int mozliweBiciePion(int x, int y, int[][] plansza) {
-        int[][] plansza_temp = copyPlansza(plansza);
-        int pionek = plansza_temp[x][y];
-        int max = 0;
-        int temp;
+    private void sprawdzRuchy(int[][] plansza_do_kopii) {
 
-        if(czyWPlanszy(x+1, y+1) && plansza_temp[x+1][y+1] != 0 && plansza_temp[x+1][y+1] % 2 != pionek % 2) {
-            if(czyWPlanszy(x+2, y+2) && plansza_temp[x+2][y+2] == 0) {
-                plansza_temp[x+1][y+1] = 0;
-                plansza_temp[x][y] = 0;
-                plansza_temp[x+2][y+2] = pionek;
-                max = ((temp = mozliweBiciePion(x+2, y+2, plansza_temp) + 1) > max ? temp : max);
-            }
-        }
-
-        plansza_temp =copyPlansza(plansza);
-        if(czyWPlanszy(x-1, y+1) && plansza_temp[x-1][y+1] != 0 && plansza_temp[x-1][y+1] % 2 != pionek % 2) {
-            if(czyWPlanszy(x-2, y+2) && plansza_temp[x-2][y+2] == 0) {
-                plansza_temp[x-1][y+1] = 0;
-                plansza_temp[x][y] = 0;
-                plansza_temp[x-2][y+2] = pionek;
-                max = ((temp = mozliweBiciePion(x-2, y+2, plansza_temp) + 1) > max ? temp : max);
-            }
-        }
-
-        plansza_temp = copyPlansza(plansza);
-        if(czyWPlanszy(x+1, y-1) && plansza_temp[x+1][y-1] != 0 && plansza_temp[x+1][y-1] % 2 != pionek % 2) {
-            if(czyWPlanszy(x+2, y-2) && plansza_temp[x+2][y-2] == 0) {
-                plansza_temp[x+1][y-1] = 0;
-                plansza_temp[x][y] = 0;
-                plansza_temp[x+2][y-2] = pionek;
-                max = ((temp = mozliweBiciePion(x+2, y-2, plansza_temp) + 1) > max ? temp : max);
-            }
-        }
-
-        plansza_temp = copyPlansza(plansza);
-        if(czyWPlanszy(x-1, y-1) && plansza_temp[x-1][y-1] != 0 && plansza_temp[x-1][y-1] % 2 != pionek % 2) {
-            if(czyWPlanszy(x-2, y-2) && plansza_temp[x-2][y-2] == 0) {
-                plansza_temp[x-1][y-1] = 0;
-                plansza_temp[x][y] = 0;
-                plansza_temp[x-2][y-2] = pionek;
-                max = ((temp = mozliweBiciePion(x-2, y-2, plansza_temp) + 1) > max ? temp : max);
-            }
-        }
-
-        return max;
     }
 
-    private int mozliweBicieKrol(int x, int y, int[][] plansza) {
-        int[][] plansza_temp = copyPlansza(plansza);
-        int pionek = plansza_temp[x][y];
-        int max = 0;
-        int temp;
+    int[][] kopiuj(int[][] plansza_do_kopii) {
+        int length = plansza_do_kopii.length;
+        int[][] plansza = new int[length][length];
+        for (int x = 0; x < length; x += 1) {
+            for (int y = 0; y < pokoj.getPlansza().length; y += 1) {
 
-        boolean flaga = true;
-        plansza_temp = plansza;
-        int x_dozbicia = -1, y_dozbicia = -1, i = 1;
-        while(flaga) {
-            if(!czyWPlanszy(x+i, y+i)) {
-                flaga = false;
             }
-            else if(plansza_temp[x+i][y+i] != 0 && plansza_temp[x+i][y+i] % 2 != pionek % 2) {
-                flaga = false;
-            }
-            else if(plansza_temp[x+i][y+i] == 0) {
-                if(x_dozbicia != -1) {
-                    plansza_temp[x][y] = 0;
-                    plansza_temp[x_dozbicia][y_dozbicia] = 0;
-                    plansza_temp[x+i][y+i] = pionek;
-
-                    max = ((temp = mozliweBicieKrol(x+i, y+i, plansza_temp) + 1) > max ? temp : max);
-                }
-            }
-            else {
-                if(x_dozbicia == -1) {
-                    x_dozbicia = x + i;
-                    y_dozbicia = y + i;
-                }
-                else {
-                    flaga = false;
-                }
-            }
-
-            i += 1;
         }
-
-        flaga = true;
-        plansza_temp = copyPlansza(plansza);
-        x_dozbicia = -1; y_dozbicia = -1; i = 1;
-        while(flaga) {
-            if(!czyWPlanszy(x-i, y+i)) {
-                flaga = false;
-            }
-            else if(plansza_temp[x-i][y+i] != 0 && plansza_temp[x-i][y+i] % 2 != pionek % 2) {
-                flaga = false;
-            }
-            else if(plansza_temp[x-i][y+i] == 0) {
-                if(x_dozbicia != -1) {
-                    plansza_temp[x][y] = 0;
-                    plansza_temp[x_dozbicia][y_dozbicia] = 0;
-                    plansza_temp[x-i][y+i] = pionek;
-
-                    max = ((temp = mozliweBicieKrol(x-i, y+i, plansza_temp) + 1) > max ? temp : max);
-                }
-            }
-            else {
-                if(x_dozbicia == -1) {
-                    x_dozbicia = x - i;
-                    y_dozbicia = y + i;
-                }
-                else {
-                    flaga = false;
-                }
-            }
-
-            i += 1;
-        }
-
-        flaga = true;
-        plansza_temp = copyPlansza(plansza);
-        x_dozbicia = -1; y_dozbicia = -1; i = 1;
-        while(flaga) {
-            if(!czyWPlanszy(x-i, y-i)) {
-                flaga = false;
-            }
-            else if(plansza_temp[x-i][y-i] != 0 && plansza_temp[x-i][y-i] % 2 != pionek % 2) {
-                flaga = false;
-            }
-            else if(plansza_temp[x-i][y-i] == 0) {
-                if(x_dozbicia != -1) {
-                    plansza_temp[x][y] = 0;
-                    plansza_temp[x_dozbicia][y_dozbicia] = 0;
-                    plansza_temp[x-i][y-i] = pionek;
-
-                    max = ((temp = mozliweBicieKrol(x-i, y-i, plansza_temp) + 1) > max ? temp : max);
-                }
-            }
-            else {
-                if(x_dozbicia == -1) {
-                    x_dozbicia = x - i;
-                    y_dozbicia = y - i;
-                }
-                else {
-                    flaga = false;
-                }
-            }
-
-            i += 1;
-        }
-
-        flaga = true;
-        plansza_temp = copyPlansza(plansza);
-        x_dozbicia = -1; y_dozbicia = -1; i = 1;
-        while(flaga) {
-            if(!czyWPlanszy(x+i, y-i)) {
-                flaga = false;
-            }
-            else if(plansza_temp[x+i][y-i] != 0 && plansza_temp[x+i][y-i] % 2 != pionek % 2) {
-                flaga = false;
-            }
-            else if(plansza_temp[x+i][y-i] == 0) {
-                if(x_dozbicia != -1) {
-                    plansza_temp[x][y] = 0;
-                    plansza_temp[x_dozbicia][y_dozbicia] = 0;
-                    plansza_temp[x+i][y-i] = pionek;
-
-                    max = ((temp = mozliweBicieKrol(x+i, y-i, plansza_temp) + 1) > max ? temp : max);
-                }
-            }
-            else {
-                if(x_dozbicia == -1) {
-                    x_dozbicia = x + i;
-                    y_dozbicia = y - i;
-                }
-                else {
-                    flaga = false;
-                }
-            }
-
-            i += 1;
-        }
-
-        return max;
     }
 }
