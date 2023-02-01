419755b0965a7fd8cbb8832ea8674359bebb86be
Jakub Drzewiecki <kuba.drzewiecki@icloud.com>
2023-02-01 23:06:59 +0100

    Dodałem widgety dla gier możliwych do obejrzenia.

--- src/main/java/klient/kontroler/GlownyKontroler.java
+++ src/main/java/klient/kontroler/GlownyKontroler.java
@@ -1,5 +1,6 @@
 package klient.kontroler;
 
+import entities.Gra;
 import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
@@ -161,5 +162,15 @@ public class GlownyKontroler {
 
     Widok widok = TworcaWidoku.wybierzWidok(TypyWidokow.WIDOK_GRY);
     Aplikacja.ustawNowyKorzen(widok.utworzWidok(kontroler, model_.modelGry()));
+  }
+
+  /**
+   * Metoda odpowiedzialna za utworzenie widoku,
+   * w ktorym mozna obejrzec przebieg zapisanej w bazie danych gry.
+   *
+   * @param gra Gra do obejrzenia.
+   */
+  public void obejrzyjGre(Gra gra) {
+
   }
 }
